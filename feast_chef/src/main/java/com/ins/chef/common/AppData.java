e41d81159362de1d3579f3816ea85a2afb9ec03b
fhbianling <fhbianling@163.com>
2017-02-27 10:42:08 +0800

    update

---
+++ feast_chef/src/main/java/com/ins/chef/common/AppData.java
@@ -0,0 +1,17 @@
+package com.ins.chef.common;
+
+/**
+ * author 边凌
+ * date 2017/2/27 10:21
+ * desc ${厨师端常量类}
+ */
+
+public class AppData {
+    public static class Url {
+        static String DOMAIN = "http://192.168.118.206:8080";
+        /**
+         * 厨师端入口页
+         */
+        public final static String FEAST_CHEF_HOMEPAGE = DOMAIN + "/Banjiuwan/app/page/cookLogin";
+    }
+}
